12c5bd4b58571f41fba0d48be73f99dc36ae31c1
pepi151101 <petra.ignjatovic2001@gmail.com>
2024-06-30 18:17:26 +0200

    increment, first try
    doesn't do anything

--- MyLICMPass/MyLICMPass.cpp
+++ MyLICMPass/MyLICMPass.cpp
@@ -12,6 +12,9 @@
 #include "llvm/Analysis/ValueTracking.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/Value.h"
+#include "llvm/IR/IRBuilder.h" 
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Type.h"
 #include <vector>
 
 using namespace llvm;
@@ -29,22 +32,45 @@ namespace {
             errs() << "Processing function: " << F.getName() << "\n";
 
             for (Loop *L : LI) {
-                errs() << "Loop Preheader: " << *L->getLoopPreheader()->getTerminator() << "\n";
+                BasicBlock *Preheader = L->getLoopPreheader();
+                if (!Preheader) continue;
+
+                IRBuilder<> Builder(Preheader->getTerminator());
+
+                // Create global or local variable for counting (incrementing)
+                AllocaInst *Counter = Builder.CreateAlloca(Type::getInt32Ty(F.getContext()), nullptr, "counter");
+                Builder.CreateStore(ConstantInt::get(Type::getInt32Ty(F.getContext()), 0), Counter);
+
                 std::vector<Instruction*> instructionsToMove;
+
                 for (BasicBlock *BB : L->blocks()) {
                     for (Instruction &I : *BB) {
                         errs() << "Instruction: " << I << "\n";
-                        errs() << "isDesiredInstructionType: " << isDesiredInstructionType(&I)<< "\n";
+                        errs() << "isDesiredInstructionType: " << isDesiredInstructionType(&I) << "\n";
                         errs() << "areAllOperandsConstantsOrComputedOutsideLoop: " << areAllOperandsConstantsOrComputedOutsideLoop(&I, L) << "\n";
                         errs() << "isSafeToSpeculativelyExecute: " << isSafeToSpeculativelyExecute(&I) << "\n";
                         errs() << "doesBlockDominateAllExitBlocks: " << doesBlockDominateAllExitBlocks(BB, L, &DT) << "\n";
+                        
+                        // Add increment at the end of each iteration
+                        if (isa<BranchInst>(I) && L->contains(&I)) {
+                            IRBuilder<> LoopBuilder(&I);
+                            LoadInst *LoadCounter = LoopBuilder.CreateLoad(Type::getInt32Ty(F.getContext()), Counter);
+                            Value *Inc = LoopBuilder.CreateAdd(LoadCounter, ConstantInt::get(Type::getInt32Ty(F.getContext()), 1), "inc");
+                            LoopBuilder.CreateStore(Inc, Counter);
+
+                            // Add print of the current value of Counter
+                            Value *CounterVal = LoopBuilder.CreateLoad(Type::getInt32Ty(F.getContext()), Counter);
+                            FunctionCallee DbgFunc = F.getParent()->getOrInsertFunction("llvm_dbg_value", FunctionType::get(Type::getVoidTy(F.getContext()), {Type::getInt32Ty(F.getContext())}, false));
+                            LoopBuilder.CreateCall(DbgFunc, {CounterVal});
+                        }
+
                         if (isDesiredInstructionType(&I) &&
                             areAllOperandsConstantsOrComputedOutsideLoop(&I, L) &&
                             isSafeToSpeculativelyExecute(&I) &&
                             doesBlockDominateAllExitBlocks(BB, L, &DT)) {
+                            instructionsToMove.push_back(&I);
                             Changed = true;
-                        }
-                        else if (auto *SI = dyn_cast<StoreInst>(&I)) {
+                        } else if (auto *SI = dyn_cast<StoreInst>(&I)) {
                             errs() << "Store Instruction!\n";
                             if(!isChangedAfterInstruction(SI, SI->getPointerOperand(), L)) {
                                 Value *StoredVal = SI->getValueOperand();
@@ -53,7 +79,36 @@ namespace {
                                     instructionsToMove.push_back(&I);
                                     Changed = true;
                                 } else if (isDefinedOutsideLoop(StoredVal, L)) {
-                                    //TODO:Izbrisati load i store i zameniti SI u petlji sa promenljivom koja se storeuje u SI
+                                    // Remove all Load/Store instructions and replace them with a new variable
+                                    std::vector<Instruction*> toRemove;
+                                    for (BasicBlock *InnerBB : L->blocks()) {
+                                        for (Instruction &InnerI : *InnerBB) {
+                                            if (LoadInst *LI = dyn_cast<LoadInst>(&InnerI)) {
+                                                if (LI->getPointerOperand() == SI->getPointerOperand()) {
+                                                    // Create a new variable outside the loop
+                                                    AllocaInst *NewVar = Builder.CreateAlloca(LI->getType(), nullptr, "hoistedVar");
+                                                    Builder.CreateStore(StoredVal, NewVar);
+
+                                                    // Replace all uses of Load with the new variable
+                                                    LI->replaceAllUsesWith(Builder.CreateLoad(LI->getType(), NewVar));
+                                                    toRemove.push_back(&InnerI);
+                                                }
+                                            }
+                                            if (StoreInst *StI = dyn_cast<StoreInst>(&InnerI)) {
+                                                if (StI->getPointerOperand() == SI->getPointerOperand()) {
+                                                    toRemove.push_back(&InnerI);
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                    // Remove all Load/Store instructions that were replaced
+                                    for (Instruction *Instr : toRemove) {
+                                        Instr->eraseFromParent();
+                                    }
+
+                                    // Move the Store instruction outside the loop
+                                    Builder.CreateStore(StoredVal, SI->getPointerOperand());
                                     Changed = true;
                                 }
                             }
@@ -61,7 +116,8 @@ namespace {
                         errs() << "\n\n\n";
                     }
                 }
-                for(Instruction* I : instructionsToMove) {
+
+                for (Instruction *I : instructionsToMove) {
                     errs() << "Instruction to move: " << *I << "\n";
                     errs() << "Where to move it: " << *L->getLoopPreheader()->getTerminator() << "\n";
                     I->moveBefore(L->getLoopPreheader()->getTerminator());
@@ -138,25 +194,22 @@ namespace {
             bool StartFound = false;
             for (BasicBlock *BB : L->blocks()) {
                 for (Instruction &I : *BB) {
-                    if (&I == StartInst) {
-                        StartFound = true;
-                        continue;
-                    }
-
                     if (StartFound) {
-                        if (auto *SI = dyn_cast<StoreInst>(&I)) {
+                        if (StoreInst *SI = dyn_cast<StoreInst>(&I)) {
                             if (SI->getPointerOperand() == Ptr) {
                                 return true;
                             }
                         }
                     }
+                    if (&I == StartInst) {
+                        StartFound = true;
+                    }
                 }
             }
             return false;
         }
-
     };
 }
 
 char MyLICMPass::ID = 0;
-static RegisterPass<MyLICMPass> X("my-licm", "My Loop Invariant Code Motion Pass", false, false);
+static RegisterPass<MyLICMPass> X("mylicm", "My LICM Pass", false, false);
